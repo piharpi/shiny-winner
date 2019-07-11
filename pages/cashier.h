1ab28782471a27849f739c78321f91bf64215033
Mahendrata Harpi <justharpi@gmail.com>
2019-07-11 22:58:11 +0700

    add cashier page

---
+++ pages/cashier.h
@@ -0,0 +1,14 @@
+void Route::Cashier(){
+  int opt;
+  do{
+    std::cout<<"===kasir==="<<std::endl;
+    std::cout<<"1. pembayaran"<<std::endl;
+    std::cout<<"2. kembali"<<std::endl;
+    std::cout<<std::endl<<"masukkan pilihan";std::cin>>opt;
+    if (opt==1){
+      std::system("cls");
+      // Route::Price();
+      std::cout<<std::endl;
+    }
+  }while(opt!=2);
+}
